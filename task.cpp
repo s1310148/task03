231e039c0df8869b3f23ca5fbbe9478cb5218e4f
s1310148 <s1310148@u-aizu.ac.jp>
2024-07-04 17:21:15 +0900

    HoT: initial revision

---
+++ task.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    srand(time(0));
+
+    int h = 0, t = 0;
+    
+    cout << "Tossing a coin..." << endl;
+
+    for(int i = 0; i < 3; i++){
+        bool isheads  = rand() % 2 == 0;
+
+        cout << "Round " << i << ": ";
+        if(isheads){
+            cout << "Heads" << endl;
+            h++;
+        }
+        else{
+            cout << "Tails" << endl;
+            t++;
+        }
+    }
+
+    cout << "Heads: " << h << ", Tails: " << t << endl;
+}
